4721bed7e3719ae9ab400bd15ff78fe0ff49185c
Markus Mayer <development@widemeadows.de>
2014-07-11 19:39:40 +0200

    added helper classes

---
+++ experimental-src/SmsSpam/EnumClass.cs
@@ -0,0 +1,45 @@
+﻿using BayesianClassifier;
+using JetBrains.Annotations;
+
+namespace SmsSpam
+{
+    /// <summary>
+    /// Class EnumClass.
+    /// </summary>
+    class EnumClass : ClassBase
+    {
+        /// <summary>
+        /// Gets the type.
+        /// </summary>
+        /// <value>The type.</value>
+        public MessageType Type { get; private set; }
+
+        public EnumClass(MessageType type, double probability)
+            : base(type.ToString(), probability)
+        {
+            Type = type;
+        }
+
+        /// <summary>
+        /// Indicates whether the current object is equal to another object of the same type.
+        /// </summary>
+        /// <param name="other">An object to compare with this object.</param>
+        /// <returns>true if the current object is equal to the <paramref name="other" /> parameter; otherwise, false.</returns>
+        public override bool Equals([CanBeNull] IClass other)
+        {
+            return other is EnumClass && Equals((EnumClass) other);
+        }
+
+        /// <summary>
+        /// Indicates whether the current object is equal to another object of the same type.
+        /// </summary>
+        /// <param name="other">An object to compare with this object.</param>
+        /// <returns>true if the current object is equal to the <paramref name="other" /> parameter; otherwise, false.</returns>
+        public bool Equals([CanBeNull] EnumClass other)
+        {
+            if (ReferenceEquals(other, null)) return false;
+            if (ReferenceEquals(other, this)) return true;
+            return Type.Equals(other.Type);
+        }
+    }
+}
---
+++ experimental-src/SmsSpam/Sms.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using JetBrains.Annotations;
+
+namespace SmsSpam
+{
+    /// <summary>
+    /// Class Sms. This class cannot be inherited.
+    /// </summary>
+    [DebuggerDisplay("{Type} {Message}")]
+    sealed class Sms
+    {
+        /// <summary>
+        /// Gets the type.
+        /// </summary>
+        /// <value>The type.</value>
+        public MessageType Type { get; private set; }
+
+        /// <summary>
+        /// Gets the content.
+        /// </summary>
+        /// <value>The content.</value>
+        [NotNull]
+        public string Content { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Sms"/> class.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <param name="content">The content.</param>
+        public Sms(MessageType type, [NotNull] string content)
+        {
+            Type = type;
+            Content = content;
+        }
+    }
+}
